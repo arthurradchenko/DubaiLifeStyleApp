031966216d8e4e722b245ca8cbe39e43166de263
Arthur <adamgontier898@gmail.com>
2019-07-19 12:31:43 +0300

    final responsive design

--- src/app/registration-step1/registration-step1.component.ts
+++ src/app/registration-step1/registration-step1.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import vocData from '../../environments/vocabulary.json';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration-step1',
@@ -12,7 +13,13 @@ export class RegistrationStep1Component implements OnInit {
   regFormUp: FormGroup;
   regFormDown: FormGroup;
   vocabulary: any = vocData;
-  constructor() { }
+  time: number = 900*1000;
+  constructor( private router: Router) { 
+      var timer = setInterval(() => {
+        this.time = this.time - 1000;
+        if(this.time <= 0) clearInterval(timer);
+      }, 1000);
+   }
 
   ngOnInit() {
     console.log(this.vocabulary);
@@ -41,5 +48,12 @@ export class RegistrationStep1Component implements OnInit {
       }
     ) 
   }
-
+  step2Up()
+  {
+    this.router.navigate(['/step2'], {queryParams: {'firstname': this.regFormUp.value.name,'email': this.regFormUp.value.mail}});
+  }
+  step2Down()
+  {
+    this.router.navigate(['/step2'], {queryParams: {'firstname': this.regFormDown.value.name,'email': this.regFormDown.value.mail}});
+  }
 }
--- src/app/registration-step2/registration-step2.component.ts
+++ src/app/registration-step2/registration-step2.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import vocData from '../../environments/vocabulary.json';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-registration-step2',
@@ -6,10 +10,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration-step2.component.sass']
 })
 export class RegistrationStep2Component implements OnInit {
-
-  constructor() { }
+  
+  regFormUp: FormGroup;
+  regFormDown: FormGroup;
+  vocabulary: any = vocData;
+  time: number = 900*1000;
+  name: string;
+  mail: string;
+  private queryParamsSub: Subscription;
+  
+  constructor( private route: ActivatedRoute) {
+    this.queryParamsSub = route.queryParams.subscribe(
+      (queryParam: any) => {
+          this.name = queryParam['firstname'];
+          this.mail = queryParam['email'];
+      }
+  );
+    var timer = setInterval(() => {
+      this.time = this.time - 1000;
+      if(this.time <= 0) clearInterval(timer);
+    }, 1000);
+   }
 
   ngOnInit() {
+    this.regFormUp = new FormGroup(
+      {
+        'firstname': new FormControl(this.name, [
+          Validators.required,
+          Validators.pattern('^[a-zA-Z ]+$')
+        ]),
+        'lastname': new FormControl('', [
+          Validators.required,
+          Validators.pattern('^[a-zA-Z ]+$')
+        ]),
+        'mail': new FormControl(this.mail, [
+          Validators.required,
+          Validators.pattern('^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$')
+        ]),
+        'password': new FormControl('', [
+          Validators.required,
+          Validators.pattern('^.{6,12}$')
+        ]),
+        'country': new FormControl('', Validators.required)
+      }
+    )    
+    this.regFormDown = new FormGroup(
+      {
+        'firstname': new FormControl(this.name, [
+          Validators.required,
+          Validators.pattern('^[a-zA-Z ]+$')
+        ]),
+        'lastname': new FormControl('', [
+          Validators.required,
+          Validators.pattern('^[a-zA-Z ]+$')
+        ]),
+        'mail': new FormControl(this.mail, [
+          Validators.required,
+          Validators.pattern('^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$')
+        ]),
+        'password': new FormControl('', [
+          Validators.required,
+          Validators.pattern('^[a-zA-Z ]+$')
+        ]),
+        'country': new FormControl('')
+      }
+    ) 
+  }
+  register()
+  {
+    if(this.regFormUp.valid || this.regFormDown.valid) alert(this.vocabulary.REGISTRATION.SUCCESS);
+    else alert(this.vocabulary.REGISTRATION.FAILURE);
   }
 
 }
